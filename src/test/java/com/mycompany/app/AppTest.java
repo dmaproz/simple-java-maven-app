cce4ec352b1916c929ab85dffc07ead829c63cfa
dmaproz <91570185+dmaproz@users.noreply.github.com>
2025-11-05 17:46:07 -0400

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello World! :D" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Hello World! :D\""+"is " + outContent.toString());
         }
     }
 
